fab3cb9d93ee903921fdb334046a5aedd8f1c2d8
lupyana <lupyanambembati@gmail.com>
2024-05-08 20:23:06 +0300

    fix: add nginx config

--- pythontz/urls.py
+++ pythontz/urls.py
@@ -3,7 +3,7 @@
 from pythontz.views import Home
 
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
